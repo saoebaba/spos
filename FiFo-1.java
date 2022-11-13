439694558f6c6b384c394f2b60fa5ac4f9daa15c
saoebaba <117111950+saoebaba@users.noreply.github.com>
2022-11-13 23:09:27 +0530

    Update FiFo-1.java

--- FiFo-1.java
+++ FiFo-1.java
@@ -1,7 +1,6 @@
-package FiFo;
-import java.util.LinkedList;		/*  	Name :- Shivam Bendre		Roll No:- COTA16 			Division:- "A1"    */
-import java.util.Queue;
-public class FiFo {
+
+import java.util.*;
+public class FIFO {
     public static void main(String[] args) {
         Queue<String> queue = new LinkedList<String>();
         queue.add("486");		/// adding values
@@ -9,16 +8,11 @@ public class FiFo {
         queue.add("352");
         queue.add("965");
         queue.add("169");
-        System.out.println("Created Queue: " + queue);
-        String value = (String) queue.remove();		
-        //   removing start element of queue 
-        System.out.println("The element deleted from the head is: " + value);
-        System.out.println("The queue after deletion: " + queue);	
-        ///printing the queue after removing 1st element
-        String head = (String) queue.peek();
-        System.out.println("The head of the present queue is: " + head);		
-        // queue start element
-        int size = queue.size();
-        System.out.println("The size of the queue is: " + size);		// printing queue size
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Created Queue: \t" + queue);
+        System.out.println("The element deleted from the head is: \t" + queue.remove());
+        System.out.println("The queue after deletion: \t" + queue);	
+        System.out.println("The head of the present queue is: \t" + queue.peek());		
+        System.out.println("The size of the queue is: \t" + queue.size());
     }
 }
